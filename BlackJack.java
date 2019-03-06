acf1b0cfe2998caf39063641dd4e6bb901ff5fd0
cedric <casas.cedric@gmail.com>
2019-03-06 10:12:42 +0100

    Deuxiéme jet

--- BlackJack.java
+++ BlackJack.java
@@ -7,7 +7,6 @@ class BlackJack {
 	public static double getCard (double min , double max){
 	
 	double card = (int)(Math.random()*((10 - 1)+1))+1;
-
 	
 	
 	return card;
@@ -15,11 +14,20 @@ class BlackJack {
 	}
 
 	public static void main(String[] args) {
-		int firstCard = (int)(getCard(1,10));
-		System.out.println(firstCard);
-			
+		int firstCardPlayerHand = (int)(getCard(1,10));
+		int secondCardPlayerHand = (int)(getCard(1,10));
+		int startPlayerHand = (firstCardPlayerHand + secondCardPlayerHand);
+		System.out.println(firstCardPlayerHand);
+		System.out.println(secondCardPlayerHand);
+		System.out.println(startPlayerHand);
+
+		int firstCardBankHand = (int)(getCard(1,10));
+		int secondCardBankHand = (int)(getCard(1,10));
+		int startBankHand = (firstCardBankHand + secondCardBankHand);
+		System.out.println(firstCardBankHand);
+		System.out.println(secondCardBankHand);
+		System.out.println(startBankHand);
 		
-	
 	}
 
 
