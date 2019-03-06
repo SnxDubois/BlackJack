f6a4e545be7c4732d698aa6f80b0e767213b45ac
cedric <casas.cedric@gmail.com>
2019-03-06 11:33:42 +0100

    3éme jet

--- BlackJack.java
+++ BlackJack.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 import java.lang.Math;
 import java.util.Random;
 
-class BlackJack {
-
+class BlackJack { 
+	//Methode pour tirer une carte aléatoirement(random)//
 	public static double getCard (double min , double max){
 	
-	double card = (int)(Math.random()*((10 - 1)+1))+1;
+		double card = (int)(Math.random()*((10 - 1)+1))+1;
 	
 	
-	return card;
+		return card;
 
 	}
-
+	
 	public static void main(String[] args) {
 		int firstCardPlayerHand = (int)(getCard(1,10));
 		int secondCardPlayerHand = (int)(getCard(1,10));
@@ -27,12 +27,40 @@ class BlackJack {
 		System.out.println(firstCardBankHand);
 		System.out.println(secondCardBankHand);
 		System.out.println(startBankHand);
+
+		System.out.println("Joueur1 décidez vous de prendre une carte? 1 : oui / 2 : non ");
+
+		Scanner myObj = new Scanner(System.in);		
+		int userInput = myObj.nextInt();
+		
+		
+
+		
+		if (userInput == 1) {
+			int thirdCardPlayerHand = (int)(getCard(1,10));
+			System.out.println(thirdCardPlayerHand);
+			int instantHand = (startPlayerHand + thirdCardPlayerHand);
+			System.out.println("Votre main est de " + instantHand + " points.");
+		
+
+		} else {
+			System.out.println("Pas de nouvelle carte");
+		}
+		
 		
 	}
 
-
-
+	
 
 
 
 }
+	
+
+	
+
+
+
+
+
+
